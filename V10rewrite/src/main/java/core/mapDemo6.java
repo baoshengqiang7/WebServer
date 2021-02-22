ab38e485e63804f7af45bb56b7cd2c346790dd8f
baoshengqiang <2310529728@qq.com>
2021-02-22 23:51:08 +0800

    V12

---
+++ V10rewrite/src/main/java/core/mapDemo6.java
@@ -0,0 +1,43 @@
+package core;
+
+import javax.imageio.stream.ImageInputStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class mapDemo6 {
+    public static void main(String[] args) {
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        map.put("迪达拉",99);
+        map.put("斑",98);
+        map.put("佐助",97);
+        map.put("六道",96);
+        map.put("佩恩",95);
+        System.out.println(map);
+
+        Set<String> set=map.keySet();
+        for (String t:set) {
+            System.out.println("key:"+t);
+        }
+
+        Collection<Integer> i=map.values();
+        for (Integer in:i) {
+            System.out.println("value:"+in);
+        }
+
+        Set<Map.Entry<String,Integer>> entrySet=map.entrySet();
+        for (Map.Entry<String,Integer> e:entrySet) {
+            String key=e.getKey();
+            int value=e.getValue();
+            System.out.println(key+":"+value);
+        }
+
+        map.forEach(
+                (k,v)->{
+                    String line=k+": "+v;
+                    System.out.println(line);
+                }
+        );
+    }
+}
